6b10b1c1f69d5ceb933cf84018b70b8ef2181582
MxVndI <kingknife29@gmail.com>
2025-03-28 23:46:08 +0300

    add lab 1 task 3

--- Func/Program.fs
+++ Func/Program.fs
@@ -1,4 +1,5 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System
+
 printfn "Hello from F#"
 
 type SolveResult =
@@ -15,5 +16,33 @@ let solve a b c =
         if D<0 then None
         else Quadratic(((-b+sqrt(D))/(2.* a), (-b-sqrt(D))/(2.*a)))
 
-let res = solve 1. 2. -3.
-System.Console.WriteLine(res)
+let circleArea r =
+    Math.PI * r * r
+
+let cylinderVolume area height =
+     area * height * 1.
+
+let volumeCylinderSuperPos =
+    circleArea >> cylinderVolume
+
+let volumeCylinderCurr radius height =
+    (circleArea radius) * height
+
+[<EntryPoint>]
+let main argv =
+    let res = solve 1. 2. -3.
+    System.Console.WriteLine(res)
+
+    System.Console.WriteLine("Введите радиус круга:")
+    let radius = float (Console.ReadLine()) |> float
+    
+    System.Console.WriteLine("Введите высоту цилиндра:")
+    let height = float (Console.ReadLine()) |> float
+
+    Console.WriteLine($"Площадь круга: {circleArea radius}")
+    Console.WriteLine($"Объем цилиндра: {volumeCylinderSuperPos radius height} \n")
+
+    Console.WriteLine($"Площадь круга: {circleArea radius}")
+    Console.WriteLine($"Объем цилиндра: {volumeCylinderCurr radius height}")
+    0
+    
