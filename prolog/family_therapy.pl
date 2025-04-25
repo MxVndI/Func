73d9ab9767b74748486dca8ec4f5cfc927d4a5be
MxVndI <kingknife29@gmail.com>
2025-04-25 19:09:01 +0300

    feat: add prolog task 2 lab 1

--- prolog/family_therapy.pl
+++ prolog/family_therapy.pl
@@ -1,5 +1,17 @@
-father(X, Y)  :- man(X), dite(Y, X), false.
-fathers(X) :- dite(X, Y), man(Y), write(Y), false.
+father(X, Y)  :- man(X), dite(Y, X).
+fathers(X) :- dite(X, Y), man(Y), write(Y). 
+
+parent(X, Y) :- dite(Y, X).
 
 sister(X, Y) :- woman(X), X\=Y, dite(X,Z), dite(Y,Z).
 sister(X) :-  dite(X,Z), dite(Y,Z), Y\=X, woman(Y), write(Y).
+
+brother(X, Y) :- man(X), X \= Y, dite(X, P), dite(Y, P).
+
+grand_so(X, Y) :- dite(X, Z), dite(Z,Y).
+grand_so(X) :- dite(Y, X), dite(Z, Y), write(Z).
+
+grand_pa_and_son(X, Y) :- grand_so(X, Y); grand_so(Y, X).
+
+plemyanik(X, Y) :- man(X), dite(X, Z), (sister(Z, Y); brother(Z, Y)), X \= Y.
+plemyanik(X) :- man(X), dite(X, Z), (sister(Z, Y); brother(Z, Y)), X \= Y, write(Y).
